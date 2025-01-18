73c64f00df7b7d72c0344c22e6ff3206a5551d96
Gayatri V <155874924+gayatriiv@users.noreply.github.com>
2025-01-18 07:52:13 +0530

    Create switch.cpp

---
+++ C++ basics/switch.cpp
@@ -0,0 +1,27 @@
+int day;
+cin >> day;
+switch(day){
+  case 1: 
+         cout << "monday";
+         break;  
+  case 1: 
+         cout << "tuesday";
+         break;
+  case 1: 
+         cout << "wednesday";
+         break;  
+  case 1: 
+         cout << "thursday";
+         break;  
+  case 1: 
+         cout << "friday";
+         break;  
+  case 1: 
+         cout << "saturday";
+         break;
+  default :
+         cout << "invalid"
+  }
+cout << "check";
+return 0;
+}
