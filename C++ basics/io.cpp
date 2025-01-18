8f2c4d54173b62b8ca1ecd45762a73a2fb3f68ea
Gayatri V <155874924+gayatriiv@users.noreply.github.com>
2025-01-18 07:43:46 +0530

    Create io.cpp

---
+++ C++ basics/io.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<math.h>
+#include<string.h>
+int main(){
+  std:: cout << "Hello world" << endl;
+  return 0;
+}
+
+// using namespace
+
+#include<iostream>
+using namespace std;
+int x;
+
+int main(){
+  cin >> x;
+  cout << "value of x: " < x;
+  return 0;
+}
+
+
+
+
