7c3b589601d98c11fb6e4ad244712de8c4610be0
Gayatri V <155874924+gayatriiv@users.noreply.github.com>
2025-01-18 07:46:48 +0530

    Create datatypes.cpp

---
+++ C++ basics/datatypes.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string.h>
+int main(){
+  string s1,s2;
+  cin >> s1 >> s2;
+  cout << s1 << " " <<s2;
+  return 0;
+}
+
+// to print all the strings together
+
+string str;
+getline(cin,str);    // takes input and stores in str.
+cout << str;
+return 0;
+
+// characters
+
+char ch;
+cin >> ch;
+cout << ch;
+
+
