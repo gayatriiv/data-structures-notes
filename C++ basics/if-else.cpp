dfa71f077faadebcc675a7ec4e7a018dd92eb69f
Gayatri V <155874924+gayatriiv@users.noreply.github.com>
2025-01-18 07:49:28 +0530

    Create if-else.cpp

---
+++ C++ basics/if-else.cpp
@@ -0,0 +1,14 @@
+int main(){
+  int marks;
+  cin >> marks;
+  if (marks < 25){
+      cout << "F" ;
+  }
+  else if (marks <= 44){
+      cout << "E" ;
+  }
+  else if (marks <= 49){
+      cout << "D";
+  }
+  return 0;
+}
